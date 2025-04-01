5b3c1d9f1fd081e1602932102cf34855cb48960c
tranvietthang251 <130987106+tranviethang251@users.noreply.github.com>
2025-04-01 10:08:16 +0700

    Update Filter
    Correct Memory code

--- RTL/Filter_2.v
+++ RTL/Filter_2.v
@@ -5,7 +5,7 @@ module Filter_2
 				        VER_COUNT_PARA  = 12,
 				        HEIGHT_IMG_PARA = 512, 
 				        WIDTH_IMG_PARA  = 512,
- 				        WEIGHT = 2 
+ 				        WEIGHT_PARA = 3 
  )
  (
   input wire                       clk ,
@@ -21,6 +21,7 @@ module Filter_2
 // Initilization WIDHT_IMAGE and HEIGHT_IMAGE
 wire [ADDR_PARA-1:0] WIDTH_IMAG       = WIDTH_IMG_PARA;
 wire [VER_COUNT_PARA-1:0] HEIGHT_IMAG = HEIGHT_IMG_PARA; 
+wire [WEIGHT_PARA-1:0]  WEIGHT = 2 ; 
 // delay i_hav and i_vav 
 reg d_i_hav ; 
 reg d_i_vav ; 
@@ -88,34 +89,36 @@ else begin
   end 
 end
 // SRAM controller ;
-always@(posedge clk or negedge rstb)  begin 
+// address counter 
+always@(posedge clk or negedge rstb) begin 
    if(~rstb) ram_addr <= 0 ; 
-	 else begin 
-	      if(wr_en) begin 
-	      ram_1[ram_addr]   <=  d_data_in ; 
-		    ram_2[ram_addr]   <=  w_s4 ;                                               // w_s4 = ram_1[ram_addr]
-        ram_addr          <= (ram_addr==(WIDTH_IMAG-1'b1)) ? 0 : ram_addr + 1'b1; // ram_addr == (WIDTH_IMAG - 1) to prevent ram_addr from going out of the range [0, WIDTH - 1]
+   else if (wr_en) ram_addr <= (ram_addr==(WIDTH_IMAG-1'b1)) ? 0 : ram_addr + 1'b1; // ram_addr == (WIDTH_IMAG - 1) to prevent ram_addr from going out of the range [0, WIDTH - 1]
+end 
+// ram 
+always@(posedge clk )  begin 
+if(wr_en) begin 
+	      ram_1[ram_addr] <=  d_data_in ; 
+	      ram_2[ram_addr]   <=  w_s4 ;                                               // w_s4 = ram_1[ram_addr
 	     end 
-    end 
 end  
 // delay flip flop 
 always@(posedge clk or negedge rstb) 
    begin 
        if(~rstb) begin 
-			 s1<=0 ;
-			 s2<=0; 
-			 s3<=0;
-			 s4<=0;
-			 s5<=0;
-			 s6<=0 ;
+          s1<=0 ;
+          s2<=0; 
+	  s3<=0;
+	  s4<=0;
+	  s5<=0;
+	  s6<=0 ;
      end 
    else if (wr_en) begin 
-       s1 <= d_data_in ; 
-       s2 <= s1 ; 
-	     s3 <= w_s4 ;
-       s4 <= s3 ; 
-	     s5 <= w_s7; 
-       s6 <= s5 ; 
+          s1 <= d_data_in ; 
+          s2 <= s1 ; 
+	  s3 <= w_s4 ;
+          s4 <= s3 ; 
+	  s5 <= w_s7; 
+          s6 <= s5 ; 
      end 
    end 
 // wire 
@@ -173,9 +176,9 @@ always@(*) begin
   case(curr_state) 
    4'b0000 : begin  
        if(~wr_en) next_state = 4'b0000 ;                 // no data input 
-       else if (ver_counter==0) next_state =  4'b0000;   // the first row are writing to ram_1 so we don't process.
-       else if (ver_counter==1) next_state =  4'b0001;   // start processing the image from the first row.
-       else if (ver_counter>=2) next_state =  4'b0100;   // start processing the pixels from the second image row to the (W)th image row 
+       else if (ver_counter==1'b0) next_state =  4'b0000;   // the first row are writing to ram_1 so we don't process.
+       else if (ver_counter==1'b1) next_state =  4'b0001;   // start processing the image from the first row.
+       else if (ver_counter>=2'd2) next_state =  4'b0100;   // start processing the pixels from the second image row to the (W)th image row 
 	     else next_state = 4'b0000; 
 	     lst_tick = 0 ;                                    // the last row are not writing to ram
    end
@@ -201,28 +204,28 @@ always@(*) begin
    end
   4'b0110: begin // xu li diem anh cuoi cung cua dong anh 
        next_state = (ver_counter==HEIGHT_IMAG-1) ? 4'b1111 : 4'b0000;   // Check the last row 
-	     lst_tick    = 0 ;                                                // if ver_counter = 0 at this state means that the last row wrote to the ram_1. 
+       lst_tick    = 0 ;                                                // if ver_counter = 0 at this state means that the last row wrote to the ram_1. 
    end
   4'b1111 : begin                                                       
-       next_state = (d_i_vav==1)? 4'b1111 : 4'b0111 ;                   // wait for i_vav = 0, then it process the last row of image in the next state
-       lst_tick   = (d_i_vav==0)? 1 : 0 ;                                // if i_vav = 0 then lst_tick = 1 to enable process pixels of last row.
+       next_state = (d_i_vav==1'b1)? 4'b1111 : 4'b0111 ;                   // wait for i_vav = 0, then it process the last row of image in the next state
+       lst_tick   = (d_i_vav==1'b0)? 1'b1 : 1'b0 ;                                // if i_vav = 0 then lst_tick = 1 to enable process pixels of last row.
    end 
   4'b0111 : begin 
        next_state = 4'b1000 ;  
-	     lst_tick = 1;
+       lst_tick = 1'b1;
    end 
   4'b1000: begin 
       next_state =(ram_addr!=WIDTH_IMAG-1) ? 4'b1000 : 4'b1001 ;      // Check whether the next pixel is the end of the row
-	    lst_tick   = 1 ; 
+      lst_tick   = 1'b1 ; 
    end 
   4'b1001 : begin                                                     // finish a image 
        next_state = 4'b0000 ; 
-       lst_tick = 0 ;                                                 // "turn off" lst_tick to stop writing to SRAM or writing to SRAM.
+       lst_tick = 1'b0 ;                                                 // "turn off" lst_tick to stop writing to SRAM or writing to SRAM.
    end 
      default : 
 	begin 
 	    next_state =4'b0000; 
-	    lst_tick   = 0 ; 
+	    lst_tick   = 1'b0; 
 	end
 endcase 
 end
@@ -341,44 +344,6 @@ always@(*) begin
    end 
   endcase 
  end 
-// 
-// delay o_s to 1 cycle clock , to limit timing fail in implementation stages.
-reg wr_2_file_0 ;
-reg [DATA_WIDTH-1:0] d_o_s1 ;
-reg [DATA_WIDTH-1:0] d_o_s2 ;
-reg [DATA_WIDTH-1:0] d_o_s3 ;
-reg [DATA_WIDTH-1:0] d_o_s4 ;
-reg [DATA_WIDTH-1:0] d_o_s5 ;
-reg [DATA_WIDTH-1:0] d_o_s6 ;
-reg [DATA_WIDTH-1:0] d_o_s7 ;
-reg [DATA_WIDTH-1:0] d_o_s8 ;
-reg [DATA_WIDTH-1:0] d_o_s9 ;
-always@(posedge clk or negedge rstb) begin 
- if(!rstb) begin 
-  d_o_s1 <= 0 ; 
-  d_o_s2 <= 0 ; 
-  d_o_s3 <= 0 ; 
-  d_o_s4 <= 0 ; 
-  d_o_s5 <= 0 ; 
-  d_o_s6 <= 0 ; 
-  d_o_s7 <= 0 ; 
-  d_o_s8 <= 0 ; 
-  d_o_s9 <= 0 ; 
-  wr_2_file_0 <= 0 ;
-  end 
-  else begin 
-  d_o_s1 <= o_s1 ; 
-  d_o_s2 <= o_s2 ; 
-  d_o_s3 <= o_s3 ; 
-  d_o_s4 <= o_s4 ; 
-  d_o_s5 <= o_s5 ; 
-  d_o_s6 <= o_s6 ; 
-  d_o_s7 <= o_s7 ; 
-  d_o_s8 <= o_s8 ; 
-  d_o_s9 <= o_s9 ;
-  wr_2_file_0 <= (|curr_state)&(|(~curr_state))   ;    // curr_state is neither 1111 nor 0000 => write to text file 
-  end 
-  end 
 // Processing the arithmetic operation 
 // we first group pair of o_s[n] to process addition operation 
 // internal register 
@@ -391,7 +356,7 @@ reg [DATA_WIDTH-1:0] reg_s5_1 ; // reg_s5_1 <= o_s5 , delay o_s5 to 1 cycle cloc
 reg wr_2_file_1 ; 
 always@(posedge clk or negedge rstb) begin 
   if(!rstb) wr_2_file_1 <=0 ;
-  else wr_2_file_1 <= wr_2_file_0 ; 
+  else wr_2_file_1 <= (|curr_state)&(|(~curr_state)) ; 
   end 
 // body 
 always@(posedge clk or negedge rstb) begin 
@@ -403,11 +368,11 @@ always@(posedge clk or negedge rstb) begin
   reg_s5_1     <= 0 ;
 end 
   else begin 
-  sum_1        <= d_o_s1 + d_o_s2 ; 
-  sum_2        <= d_o_s3 + d_o_s4 ; 
-  sum_3        <= d_o_s6 + d_o_s7 ; 
-  sum_4        <= d_o_s8 + d_o_s9 ; 
-  reg_s5_1     <= d_o_s5 ; 
+  sum_1        <= o_s1 + o_s2 ; 
+  sum_2        <= o_s3 + o_s4 ; 
+  sum_3        <= o_s6 + o_s7 ; 
+  sum_4        <= o_s8 + o_s9 ; 
+  reg_s5_1     <= o_s5 ; 
   end
   end 
 // we continue group pairs to process
@@ -416,7 +381,7 @@ reg [DATA_WIDTH+1:0] sum_3_4 ;   // sum_3_4 <= sum_3 + sum_4 ;
 reg [DATA_WIDTH-1:0] reg_s5_2 ;  // reg_s5_2 <= reg_s5_1 ;
 reg wr_2_file_2 ; 
 always@(posedge clk or negedge rstb) begin 
-  if(!rstb) wr_2_file_1 <=0 ;
+  if(!rstb) wr_2_file_2 <=0 ;
   else wr_2_file_2 <= wr_2_file_1 ; 
   end
 always@(posedge clk or negedge rstb) begin 
@@ -425,20 +390,14 @@ always@(posedge clk or negedge rstb) begin
     sum_3_4 <=  0 ; 
     reg_s5_2 <= 0 ; 
   end 
-
  else begin 
-
   sum_1_2 <= sum_1+sum_2 ; 
   sum_3_4 <=  sum_3+sum_4 ; 
   reg_s5_2 <= reg_s5_1 ; 
-
   end
-
  end
-
  //
-
-reg[DATA_WIDTH+2:0] o_sum ; // o_sum <= sum_1_2 + sum_3_4 ; 
+ reg[DATA_WIDTH+2:0] o_sum ; // o_sum <= sum_1_2 + sum_3_4 ; 
  reg[DATA_WIDTH-1:0] reg_s5_3 ; // 
  reg wr_2_file_3 ; 
  always@(posedge clk or negedge rstb) begin 
