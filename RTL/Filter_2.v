94ab406395c1adcb697999cc616bbb3e331f1397
tranvietthang251 <130987106+tranviethang251@users.noreply.github.com>
2025-04-07 23:21:34 +0700

    Implementation mean Filter using block ram
    Last week, I find out information about the difference between LUTRAM and BLOCKRAM, I hoped that both of them have the same attributes. Unfortunately, LUTRAM is distributed enabling read and write asynchronously. Meanwhile, If you want to infer Block ram, you will write and read synchronously. This cause many problems, because my design base on SRAM with asynchronous read and write operation. As a result, I have to fine tune my design to infer to block ram.

--- RTL/Filter_2.v
+++ RTL/Filter_2.v
@@ -1,6 +1,6 @@
 module Filter_2
  # (
-    parameter   DATA_WIDTH      = 8, 
+    parameter           DATA_WIDTH      = 8, 
 				        ADDR_PARA       = 12,
 				        VER_COUNT_PARA  = 12,
 				        HEIGHT_IMG_PARA = 512, 
@@ -21,13 +21,12 @@ module Filter_2
 // Initilization WIDHT_IMAGE and HEIGHT_IMAGE
 wire [ADDR_PARA-1:0] WIDTH_IMAG       = WIDTH_IMG_PARA;
 wire [VER_COUNT_PARA-1:0] HEIGHT_IMAG = HEIGHT_IMG_PARA; 
-wire [WEIGHT_PARA-1:0]  WEIGHT = 2 ; 
-// delay i_hav and i_vav 
-reg d_i_hav ; 
-reg d_i_vav ; 
+wire [WEIGHT_PARA-1:0]  WEIGHT = WEIGHT_PARA ; 
 // delay input 
-reg [DATA_WIDTH-1:0] d_data_in ; 
-// 6 delay register 
+// delay register 
+reg [DATA_WIDTH-1:0] reg_data_in_1; 
+reg [DATA_WIDTH-1:0] reg_data_in_2 ; 
+reg [DATA_WIDTH-1:0] reg_do_ram_1 ; 
 reg [DATA_WIDTH-1:0] s1 ;  
 reg [DATA_WIDTH-1:0] s2 ;
 reg [DATA_WIDTH-1:0] s3 ; 
@@ -45,7 +44,8 @@ wire [DATA_WIDTH-1:0] w_s7 ;
 wire [DATA_WIDTH-1:0] w_s8 ;
 wire [DATA_WIDTH-1:0] w_s9 ;
 // address register
-reg [ADDR_PARA-1:0] ram_addr ; //  ram_addr = [0 -> width of image-1] 
+reg [ADDR_PARA-1:0] ram_addr_1 ; // read and write address for ram 1,  ram_addr_1 = [0 -> width of image-1] 
+reg [ADDR_PARA-1:0] ram_addr_2 ; // read and write address for ram 2,  ram_addr_2 = [0 -> width of image-1] 
 // Vertical counter 
 reg [VER_COUNT_PARA-1:0] ver_counter ; 
 // output register 
@@ -60,10 +60,6 @@ reg [DATA_WIDTH-1:0] o_s8 ;
 reg [DATA_WIDTH-1:0] o_s9 ;
 // filtered result 
 reg [DATA_WIDTH-1:0] mean_filter ;  
-// edge resutl 
-reg [DATA_WIDTH:0] edge_point ; 
-// enhanced result 
-reg [17:0] en_img ; 
 // last tick 
 reg lst_tick ;
 // enable signal
@@ -73,66 +69,96 @@ reg de_i_hav ;
 // negative edge signal of i_hav
 wire neg_i_hav;
 // SRAM declaration 
-reg [DATA_WIDTH-1:0]ram_1[WIDTH_IMG_PARA-1:0] ; // the length of SRAM = WIDTH of image. It stores DATA_WIDTH bits for each image point
-reg [DATA_WIDTH-1:0]ram_2[WIDTH_IMG_PARA-1:0] ; // the length of SRAM = WIDTH of image. It stores DATA_WIDTH bits for each image point
+(* ram_style = "block" *) reg [DATA_WIDTH-1:0]ram_1[WIDTH_IMG_PARA-1:0] ; // the length of SRAM = WIDTH of image. It stores DATA_WIDTH bits for each image point
+(* ram_style = "block" *) reg [DATA_WIDTH-1:0]ram_2[WIDTH_IMG_PARA-1:0] ; // the length of SRAM = WIDTH of image. It stores DATA_WIDTH bits for each image point
+reg [DATA_WIDTH-1:0] dout_ram_1 ; 
+reg [DATA_WIDTH-1:0] dout_ram_2 ; 
 // body 
 always@(posedge clk or negedge rstb) begin 
   if(!rstb) begin 
-    d_i_hav   <= 0 ;
-    d_i_vav   <= 0 ;
-    d_data_in <= 0 ;
+     reg_data_in_1 <= 0 ; 
+     reg_data_in_2 <= 0 ;
   end 
 else begin
-    d_i_hav   <= i_hav ;
-    d_i_vav   <= i_vav ; 
-    d_data_in <= data_in ; 
+    reg_data_in_1<= data_in ; 
+    reg_data_in_2<= reg_data_in_1 ; 
   end 
 end
 // SRAM controller ;
 // address counter 
+// ram_1 address ; 
 always@(posedge clk or negedge rstb) begin 
-   if(~rstb) ram_addr <= 0 ; 
-   else if (wr_en) ram_addr <= (ram_addr==(WIDTH_IMAG-1'b1)) ? 0 : ram_addr + 1'b1; // ram_addr == (WIDTH_IMAG - 1) to prevent ram_addr from going out of the range [0, WIDTH - 1]
+   if(~rstb) begin 
+    ram_addr_1 <= 0 ; 
+  end 
+   else if (wr_en) begin 
+   ram_addr_1 <= (ram_addr_1==(WIDTH_IMAG-1'b1)) ? 0 : ram_addr_1 + 1'b1; // ram_addr == (WIDTH_IMAG - 1) to prevent ram_addr from going out of the range [0, WIDTH - 1]
+ end 
+end 
+// ram 2 write enable signal 
+reg wr_en_ram_2 ; // wr_en_ram_2 <= wr_en ;
+always@(posedge clk or negedge rstb) begin 
+   if(~rstb) wr_en_ram_2 <= 0 ; 
+   else wr_en_ram_2      <= wr_en ; 
+end 
+// ram_2 address ; 
+always@(posedge clk or negedge rstb) begin 
+   if(~rstb) begin 
+    ram_addr_2 <= 0 ; 
+  end 
+   else if (wr_en_ram_2) begin 
+    ram_addr_2 <=  ram_addr_1 ; 
+ end 
 end 
 // ram 
+// ram 1 controller 
 always@(posedge clk )  begin 
-if(wr_en) begin 
-	      ram_1[ram_addr] <=  d_data_in ; 
-	      ram_2[ram_addr]   <=  w_s4 ;                                               // w_s4 = ram_1[ram_addr
-	     end 
+   if(wr_en) begin 
+	    ram_1[ram_addr_1] <=  data_in ;
+	    dout_ram_1      <= ram_1[ram_addr_1] ;                                     // w_s4 = ram_1[ram_addr
+	     end
 end  
+//ram_2 controller 
+always@(posedge clk ) begin 
+   if(wr_en_ram_2) begin 
+     ram_2[ram_addr_2] <= dout_ram_1 ; 
+     dout_ram_2       <= ram_2[ram_addr_2] ;
+   end 
+ end 
 // delay flip flop 
-always@(posedge clk or negedge rstb) 
-   begin 
-       if(~rstb) begin 
-          s1<=0 ;
-          s2<=0; 
-	  s3<=0;
-	  s4<=0;
-	  s5<=0;
-	  s6<=0 ;
-     end 
-   else if (wr_en) begin 
-          s1 <= d_data_in ; 
+always@(posedge clk or negedge rstb) begin 
+ if(~rstb) begin 
+          s1 <= 0 ;
+          s2 <= 0  ;
+	        s3 <= 0  ;
+          s4 <= 0  ; 
+	        s5 <= 0  ; 
+          s6 <= 0  ;
+          reg_do_ram_1 <= 0 ; 
+ end 
+      else 
+        begin 
+          s1 <= reg_data_in_2 ;
           s2 <= s1 ; 
-	  s3 <= w_s4 ;
+	        s3 <= w_s4 ;
           s4 <= s3 ; 
-	  s5 <= w_s7; 
+	        s5 <= w_s7 ;  
           s6 <= s5 ; 
+          reg_do_ram_1 <= dout_ram_1 ; 
      end 
    end 
 // wire 
-assign w_s1 = d_data_in ; 
+assign w_s1 = reg_data_in_2 ;  
 assign w_s2 = s1  ;
 assign w_s3 = s2  ;
-assign w_s4 = ram_1[ram_addr] ;
+assign w_s4 = reg_do_ram_1 ;   
 assign w_s5 = s3; 
-assign w_s6 = s4; 
-assign w_s7 = ram_2[ram_addr] ; 
+assign w_s6 = s4;   
+assign w_s7 = dout_ram_2 ; 
 assign w_s8 = s5 ; 
 assign w_s9 = s6 ; 
 // enable signal 
-assign wr_en = (d_i_hav&d_i_vav)|(lst_tick);   
+assign wr_en = (i_hav&i_vav)|(lst_tick);   
 // verical counter used to count the row of image which is useful to decide the state of machine.
 // verical counter 
 always@(posedge clk or negedge rstb) begin 
@@ -142,10 +168,65 @@ always@(posedge clk or negedge rstb) begin
 //  delay d_i_hav to 1 cycle clock 
 always@(posedge clk or negedge rstb) begin 
     if(~rstb) de_i_hav <=0 ; 
-    else de_i_hav <= d_i_hav ; 
+    else de_i_hav <= i_hav ; 
     end 
-assign neg_i_hav = (~d_i_hav)&de_i_hav; 
+assign neg_i_hav = (~i_hav)&de_i_hav; 
+// signal for FSM decleration 
+reg [VER_COUNT_PARA-1:0] vav_pos ; 
+reg [ADDR_PARA-1:0]      hav_pos ; 
+reg FSM_en  ; 
+always@(posedge clk or negedge rstb) begin 
+  if(~rstb) begin 
+    FSM_en <= 0 ; 
+    vav_pos <= 0; 
+    hav_pos <= 0 ;
+  end 
+else begin 
+    FSM_en  <= wr_en_ram_2 ; 
+    vav_pos <= ver_counter ;
+    hav_pos <= ram_addr_2 ; 
+end
+end 
 // FSM 
+// FSM for memory controller 
+// state register 
+  reg [1:0] contr_curr_state ;  
+  reg [1:0] contr_next_state ; 
+// current state logic 
+always@(posedge clk or negedge rstb) begin 
+  if(~rstb) contr_curr_state <= 2'd0 ;
+  else  contr_curr_state <= contr_next_state ; 
+end 
+// next state logic 
+always@(*) begin 
+   case(contr_curr_state) 
+     2'b00 : begin 
+     lst_tick = 0 ;
+     if(~wr_en) contr_next_state = 2'd0 ; 
+     else if (ver_counter==HEIGHT_IMAG-1) contr_next_state = 2'b01 ; 
+     else contr_next_state = 2'd0 ; 
+     end
+     2'b01 : begin 
+     if(i_vav == 0) begin 
+       lst_tick = 1'b1 ; 
+       contr_next_state = 2'b10 ; 
+       end  
+       else begin 
+       lst_tick = 1'b0 ; 
+       contr_next_state = 2'b01 ; 
+      end 
+     end
+      2'b10: begin 
+       lst_tick = 1'b1 ; 
+       contr_next_state = (ram_addr_1 == WIDTH_IMAG-1) ? 2'b00 : 2'b01 ;
+      end 
+    default : begin 
+      lst_tick = 1'b0 ; 
+      contr_next_state = 2'b00 ; 
+    end 
+    endcase 
+end 
+// FSM for filter 
 // state register
   reg [3:0] curr_state ;  // current state logic
   reg [3:0] next_state ;  // next state logic 
@@ -175,57 +256,45 @@ always@(posedge clk or negedge rstb) begin
 always@(*) begin 
   case(curr_state) 
    4'b0000 : begin  
-       if(~wr_en) next_state = 4'b0000 ;                 // no data input 
-       else if (ver_counter==1'b0) next_state =  4'b0000;   // the first row are writing to ram_1 so we don't process.
-       else if (ver_counter==1'b1) next_state =  4'b0001;   // start processing the image from the first row.
-       else if (ver_counter>=2'd2) next_state =  4'b0100;   // start processing the pixels from the second image row to the (W)th image row 
-	     else next_state = 4'b0000; 
-	     lst_tick = 0 ;                                    // the last row are not writing to ram
+       if(~FSM_en) next_state = 4'b0000 ;                 // no data input 
+       else if (vav_pos==1'b0) next_state =  4'b0000;   // the first row are writing to ram_1 so we don't process.
+       else if (vav_pos==1'b1) next_state =  4'b0001;   // start processing the image from the first row.
+       else if (vav_pos>=2'd2) next_state =  4'b0100;   // start processing the pixels from the second image row to the (W)th image row 
+	     else next_state = 4'b0000;                         
    end
    4'b0001: begin
-       next_state = 4'b0010 ;
-	     lst_tick    = 0 ;                                                // the last row are not writing to ram
+       next_state = 4'b0010 ;                                               // the last row are not writing to ram
    end
   4'b0010: begin 
-       next_state = (ram_addr!=WIDTH_IMAG-1) ?  4'b0010 : 4'b0011 ;     // Check whether the next pixel is the end of the row 
-	     lst_tick = 0 ;
+       next_state = (hav_pos!=WIDTH_IMAG-1) ?  4'b0010 : 4'b0011 ;     // Check whether the next pixel is the end of the row 
    end
   4'b0011 : begin 
-       next_state = 4'b0000 ; 
-       lst_tick = 0 ;                                                   // the last row are not writing to ram
+       next_state = 4'b0000 ;                                                 // the last row are not writing to ram
    end 
   4'b0100: begin 
        next_state = 4'b0101 ; 
-       lst_tick = 0 ;
    end 
   4'b0101 : begin  
-       next_state = (ram_addr!=WIDTH_IMAG-1) ? 4'b0101 : 4'b0110 ;      // Check whether the next pixel is the end of the row
-       lst_tick   = 0 ;                                                 // the last row are not writing to ram
+       next_state = (hav_pos!=WIDTH_IMAG-1) ? 4'b0101 : 4'b0110 ;      // Check whether the next pixel is the end of the row
    end
   4'b0110: begin // xu li diem anh cuoi cung cua dong anh 
-       next_state = (ver_counter==HEIGHT_IMAG-1) ? 4'b1111 : 4'b0000;   // Check the last row 
-       lst_tick    = 0 ;                                                // if ver_counter = 0 at this state means that the last row wrote to the ram_1. 
+       next_state = (vav_pos==0) ? 4'b1111 : 4'b0000;   // Check the last row 
    end
   4'b1111 : begin                                                       
-       next_state = (d_i_vav==1'b1)? 4'b1111 : 4'b0111 ;                   // wait for i_vav = 0, then it process the last row of image in the next state
-       lst_tick   = (d_i_vav==1'b0)? 1'b1 : 1'b0 ;                                // if i_vav = 0 then lst_tick = 1 to enable process pixels of last row.
+       next_state = (FSM_en==0)? 4'b1111 : 4'b0111 ;                   // wait for i_vav = 0, then it process the last row of image in the next state                            
    end 
   4'b0111 : begin 
        next_state = 4'b1000 ;  
-       lst_tick = 1'b1;
    end 
   4'b1000: begin 
-      next_state =(ram_addr!=WIDTH_IMAG-1) ? 4'b1000 : 4'b1001 ;      // Check whether the next pixel is the end of the row
-      lst_tick   = 1'b1 ; 
+      next_state =(hav_pos!=WIDTH_IMAG-1) ? 4'b1000 : 4'b1001 ;      // Check whether the next pixel is the end of the row
    end 
   4'b1001 : begin                                                     // finish a image 
-       next_state = 4'b0000 ; 
-       lst_tick = 1'b0 ;                                                 // "turn off" lst_tick to stop writing to SRAM or writing to SRAM.
+       next_state = 4'b0000 ;          
    end 
      default : 
 	begin 
 	    next_state =4'b0000; 
-	    lst_tick   = 1'b0; 
 	end
 endcase 
 end
@@ -425,17 +494,14 @@ always@(posedge clk or negedge rstb) begin
  always@(posedge clk or negedge rstb) begin 
    if(!rstb) begin 
      total_sum <= 0 ; 
-     reg_s5_4 <= 0 ;
    end 
  else begin 
       total_sum <= o_sum + reg_s5_3 ; 
-      reg_s5_4  <= reg_s5_3 ;
    end 
  end 
 // 
 reg [15:0] mul_sum ; // mul_sum <= {total_sum,4'd0} + {total_sum,3'd0} + {total_sum,2'd0} ;
-reg [DATA_WIDTH-1:0] reg_s5_5 ; // reg_s5_5 <= reg_s5_4 ; 
- reg wr_2_file_5 ;
+reg wr_2_file_5 ;
  always@(posedge clk or negedge rstb) begin 
    if(!rstb) wr_2_file_5<=0 ; 
    else wr_2_file_5 <= wr_2_file_4 ; 
@@ -443,16 +509,13 @@ reg [DATA_WIDTH-1:0] reg_s5_5 ; // reg_s5_5 <= reg_s5_4 ;
 always@(posedge clk or negedge rstb) begin  
   if(!rstb) begin 
      mul_sum <= 0 ; 
-     reg_s5_5 <= 0 ;
   end else 
   begin 
     mul_sum <= {total_sum,4'd0} + {total_sum,3'd0} + {total_sum,2'd0} ;
-    reg_s5_5 <= reg_s5_4 ;
     end 
 end 
 // 
 reg wr_2_file_6 ;
-reg [DATA_WIDTH-1:0] reg_s5_6 ; 
  always@(posedge clk or negedge rstb) begin 
    if(!rstb) wr_2_file_6<=0 ; 
    else wr_2_file_6 <= wr_2_file_5 ; 
@@ -460,54 +523,21 @@ reg [DATA_WIDTH-1:0] reg_s5_6 ;
  always@(posedge clk or negedge rstb) begin 
    if(!rstb) begin 
      mean_filter <= 0 ; 
-     reg_s5_6    <= 0 ; 
    end 
  else begin 
      mean_filter <= mul_sum[15:8] ; 
-     reg_s5_6 <= reg_s5_5 ;
     end 
  end 
 //
-reg [DATA_WIDTH-1:0] d_mean_filter ;  
-reg wr_2_file_7 ; 
- always@(posedge clk or negedge rstb) begin 
-   if(!rstb) wr_2_file_7<=0 ; 
-   else wr_2_file_7 <= wr_2_file_6 ; 
- end
- always@(posedge clk or negedge rstb) begin 
-   if(!rstb) begin 
-     edge_point <= 0 ;
-     d_mean_filter <= 0 ; 
-   end 
- else begin 
-     edge_point <= $signed({1'b0,reg_s5_6}) - $signed({1'b0,mean_filter}) ; 
-     d_mean_filter <= mean_filter ;
-   end 
- end 
-// 
-reg wr_2_file_8 ; 
- always@(posedge clk or negedge rstb) begin 
-   if(!rstb) wr_2_file_8<=0 ; 
-   else wr_2_file_8 <= wr_2_file_7 ; 
- end
- always@(posedge clk or negedge rstb) begin 
-   if(!rstb) begin  
-      en_img <= 0 ; 
-    end 
-  else begin 
-       en_img <= $signed({1'b0,WEIGHT})*$signed(edge_point) + $signed({1'b0,d_mean_filter})  ;
-  end 
- end 
- //
  reg wr_2_file_9 ; 
  reg [DATA_WIDTH-1:0] de_result ; 
   always@(posedge clk or negedge rstb) begin 
    if(!rstb) wr_2_file_9<=0 ; 
-   else wr_2_file_9 <= wr_2_file_8 ; 
+   else wr_2_file_9 <= wr_2_file_6 ; 
  end
  always@(posedge clk or negedge rstb) begin 
    if(!rstb) de_result <= 0 ;
-   else de_result      <=  (en_img[17]==1)? 8'd0 : (|(en_img[16:8])==1) ? 8'd255 : en_img[7:0]; 
+   else de_result      <=  mean_filter ; 
  end 
 assign wr_file = wr_2_file_9; 
  assign data_out = de_result ;
